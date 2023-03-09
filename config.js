918d18423bfe63e5d019e3c2a960c6fd8c6e7113
George Floridis <baoxyman@gmail.com>
2023-03-09 19:54:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ Kebab = []; //
 Kebab.name = "George Floridis";
 Kebab.phrase = "Stay Up";
 
-Kebab.pfp = "IMG_20230206_102034_1.jpg";  //  ui/assets/img
+Kebab.pfp = "test";  //  ui/assets/img
 Kebab.Background = "123.gif";  // ui/assets/img/bg
 Kebab.TemplateColorHex = "#FF0000";  // https://www.color-hex.com
 Kebab.Music = "audio.mp3"; // ui/assets/audio/

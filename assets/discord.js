b4af5052ac28b7cf2f52038eec665b3152681905
George Floridis <baoxyman@gmail.com>
2023-05-07 17:13:44 +0300

    Activity Info Fix

--- assets/discord.js
+++ assets/discord.js
@@ -22,8 +22,8 @@
       }else{
           document.getElementById('pfp').style.borderColor = "GRAY";
       }
-            //get Activity from lanyard
-             
+    //get Activity from lanyard
+
     // Checks If user is listening to Spotify
     const lts = data["listening_to_spotify"];
       if (lts == true) {
@@ -41,51 +41,71 @@
              document.getElementById('cover').style.display = "block";
 
           }else {
-     document.getElementById('musicPlayer-Container').style.display = "block";
+            // If user is not listening to Spotify, Then show Game Activity
+
+            document.getElementById('musicPlayer-Container').style.display = "block";
 
             document.getElementById("header-label").innerHTML = "Now Playing";
             console.log(data["activities"][0]);
-            // if(data["activities"][0] == undefined) {
-            // document.getElementById('musicPlayer-Container').style.display = "none";
+            //checks if the activity is a Custom Status, If so then is doesnt show the activity
+            if(data["activities"][0]["type"] == "4") {
+            document.getElementById('musicPlayer-Container').style.display = "none";
+            }
+            //checks if There is a CS , If there is then it checks if the second activity is not a CS,if it isnt then it shows the activity
+            if (data["activities"][0]["type"] == "4") {
+            if(data["activities"][1]["type"]<"4"){
+              document.getElementById('musicPlayer-Container').style.display = "block";
 
-            // }
-            console.log(data["activities"][0]["type"])
-            if(data["activities"][0]["type"]=="4"){
               document.getElementById("title").innerHTML = data["activities"][1]["name"];
+              //checks if the activity has a Description, if it doesn't have one then it doesnt show the activity
+              if(data["activities"][1]["details"] == undefined){
+            document.getElementById('musicPlayer-Container').style.display = "none";
+
+              }else{
              document.getElementById("artist").innerHTML = data["activities"][1]["details"];
             document.getElementById("album").innerHTML = data["activities"][1]["state"];
             
              const largeIMG = data["activities"][1]["assets"]["large_image"];
              const appID = data["activities"][1]["application_id"];
+             //checks if the activity has a Large Image to show, if it doesnt then it doesnt show the Image
              if (largeIMG == undefined){
-             document.getElementById('cover').style.display = "none";
+             document.getElementById('cover').style.display = "none"; 
              }else{
              document.getElementById('cover').style.display = "block";
              document.getElementById("cover").src = "https://cdn.discordapp.com/app-assets/"+ appID +"/"+ largeIMG;
              }
-            
-            }else{
-               document.getElementById("title").innerHTML = data["activities"][0]["name"];
-             document.getElementById("artist").innerHTML = data["activities"][0]["details"];
+              }
+            }
+            }
+            //if there isnt a CS then it doesnt go to the next activity
+            else{
+              document.getElementById("title").innerHTML = data["activities"][0]["name"];
+              //checks if the activity has a Description, if it doesn't have one then it doesnt show the activity
+
+              if(data["activities"][0]["details"] == undefined){
+            document.getElementById('musicPlayer-Container').style.display = "none";
+
+              }else{
+            document.getElementById("artist").innerHTML = data["activities"][0]["details"];
             document.getElementById("album").innerHTML = data["activities"][0]["state"];
             
              const largeIMG = data["activities"][0]["assets"]["large_image"];
              const appID = data["activities"][0]["application_id"];
+              //checks if the activity has a Large Image to show, if it doesnt then it doesnt show the Image
+
              if (largeIMG == undefined){
              document.getElementById('cover').style.display = "none";
              }else{
              document.getElementById('cover').style.display = "block";
              document.getElementById("cover").src = "https://cdn.discordapp.com/app-assets/"+ appID +"/"+ largeIMG;
              }
+              }
+
             }
            
-            // If user is not listening to Spotify, Then show Game Activity
              
              
 
           }
     }
     getData();
-
-   
-    
